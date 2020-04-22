e2b5ba68fb331e51361b6ea075554b3817fa65bc
Andrew Rutter <andrew@creativeclaritycreations.com>
2020-04-22 13:39:38 -0400

    Address info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,24 +5,27 @@ import {htmlToReact, Link} from '../utils';
 import Social from './Social';
 
 export default class Footer extends React.Component {
-    render() {
-        return (
-            <footer id="colophon" className="site-footer outer">
-              <div className="inner">
-                <div className="site-footer-inside">
-                  <p className="site-info">
-                    {htmlToReact(_.get(this.props, 'pageContext.site.siteMetadata.footer.content'))}
-                    &nbsp;
-                    {_.map(_.get(this.props, 'pageContext.site.siteMetadata.footer.links'), (link, link_idx) => (<React.Fragment key={link_idx}>
-                    <Link key={link_idx} to={_.get(link, 'url')} {...(_.get(link, 'new_window') ? {target: '_blank', rel: 'noopener'} : null)}>{_.get(link, 'text')}</Link>.
-                    </React.Fragment>))}
-                  </p>
-                  {_.get(this.props, 'pageContext.site.siteMetadata.footer.has_social') && 
-                    <Social {...this.props} />
-                  }
-                </div>
-              </div>
-            </footer>
-        );
+  render() {
+    return (<footer id="colophon" className="site-footer outer">
+      <div className="inner">
+        <div className="site-footer-inside">
+          <p>
+            Creative Clarity Inc
+          </p>
+          {_.get(this.props, 'pageContext.site.siteMetadata.footer.has_social') && <Social {...this.props}/>}
+        </div>
+      </div>
+      <div class="inner site-info">
+        <div class="site-footer-inside">
+          <p class="">11770 Haynes Bridge Rd, Ste 205-508 Alpharetta, GA 30009<br/>
+              <a href="tel:8332703304">(833) 270-3304</a>
+            </p>
+            <ul class="social-links">
+              <li>&copy;2020 All Rights Reserved.</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      );
     }
 }
