db64d985987379a64dc8695062e1e3817d37a3d1
Andrew Rutter <andrew@creativeclaritycreations.com>
2020-04-22 07:03:47 -0400

    Sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,60 +1,61 @@
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: require('./site-metadata.json'),
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-          resolve: `gatsby-plugin-google-analytics`,
-          options: {
-              trackingId: "UA-13088066-4",
-          }
-        },
-        `gatsby-source-data`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-stackbit-static-sass`,
-            options: {
-                inputFile: `${__dirname}/src/sass/main.scss`,
-                outputFile: `${__dirname}/public/assets/css/main.css`
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [`gatsby-remark-component`]
-            }
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {
-
-            }
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-                menus: require('./src/data/menus.json'),
-            }
-        },
-        {
-        resolve: `gatsby-plugin-manifest`,
-        options: {
-          name: `Creative Clarity`,
-          short_name: `Creative Clarity`,
-          start_url: `/`,
-          background_color: `#f7f0eb`,
-          theme_color: `#a2466c`,
-          display: `standalone`,
-          icon: `static/images/logo-dark.png`
-        }
+  pathPrefix: '/',
+  siteMetadata: require('./site-metadata.json'),
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-13088066-4",
       }
-    ]
+    },
+    `gatsby-source-data`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-stackbit-static-sass`,
+      options: {
+        inputFile: `${__dirname}/src/sass/main.scss`,
+        outputFile: `${__dirname}/public/assets/css/main.css`
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-component`]
+      }
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {
+
+      }
+    },
+    {
+      resolve: `@stackbit/gatsby-plugin-menus`,
+      options: {
+        sourceUrlPath: `fields.url`,
+        pageContextProperty: `menus`,
+        menus: require('./src/data/menus.json'),
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Creative Clarity`,
+        short_name: `Creative Clarity`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/images/logo-dark.png`
+      }
+    },
+    `gatsby-plugin-sitemap`
+  ]
 };
