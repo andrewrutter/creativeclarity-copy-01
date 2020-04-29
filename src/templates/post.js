ee8bd9dd0316a7b50cd3a41ea8e431ac793d1d71
Andrew Rutter <andrew@creativeclaritycreations.com>
2020-04-29 15:49:12 -0400

    Replace social sharing links

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,9 +5,61 @@ import {Helmet} from 'react-helmet';
 
 import {Layout} from '../components/index';
 import {htmlToReact, safePrefix} from '../utils';
+import {
+  FacebookShareCount,
+  PinterestShareCount,
+  VKShareCount,
+  OKShareCount,
+  RedditShareCount,
+  TumblrShareCount,
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  LinkedinShareButton,
+  TwitterShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  RedditShareButton,
+  EmailShareButton,
+  TumblrShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  ViberShareButton,
+  WorkplaceShareButton,
+  LineShareButton,
+  WeiboShareButton,
+  PocketShareButton,
+  InstapaperShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  VKIcon,
+  OKIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  RedditIcon,
+  TumblrIcon,
+  MailruIcon,
+  EmailIcon,
+  LivejournalIcon,
+  ViberIcon,
+  WorkplaceIcon,
+  LineIcon,
+  PocketIcon,
+  InstapaperIcon,
+  WeiboIcon,
+} from "react-share";
 
 export default class Post extends React.Component {
     render() {
+      console.log(this.props)
+        const shareUrl = _.get(this.props, 'location.href');
+        const title = _.get(this.props, 'pageContext.frontmatter.title');
+        const exampleImage = _.get(this.props, 'pageContext.frontmatter.content_img_path');
         return (
             <Layout {...this.props}>
               <Helmet>
@@ -40,7 +92,152 @@ export default class Post extends React.Component {
                   <div className="post-content inner-small">
                     {htmlToReact(_.get(this.props, 'pageContext.html'))}
                   </div>
-                  <div className="sharethis-inline-share-buttons"></div>
+                  <div className="social-wrappers inner-small">
+                    <div className="social-wrapper">
+                      <FacebookShareButton
+                        url={shareUrl}
+                        quote={title}
+                        className="social-wrapper__share-button"
+                      >
+                        <FacebookIcon size={32} round />
+                      </FacebookShareButton>
+
+                      <div>
+                        <FacebookShareCount url={shareUrl} className="social-wrapper__share-count">
+                          {count => count}
+                        </FacebookShareCount>
+                      </div>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <FacebookMessengerShareButton
+                        url={shareUrl}
+                        appId="521270401588372"
+                        className="social-wrapper__share-button"
+                      >
+                        <FacebookMessengerIcon size={32} round />
+                      </FacebookMessengerShareButton>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <TwitterShareButton
+                        url={shareUrl}
+                        title={title}
+                        className="social-wrapper__share-button"
+                      >
+                        <TwitterIcon size={32} round />
+                      </TwitterShareButton>
+
+                      <div className="social-wrapper__share-count">&nbsp;</div>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <TelegramShareButton
+                        url={shareUrl}
+                        title={title}
+                        className="social-wrapper__share-button"
+                      >
+                        <TelegramIcon size={32} round />
+                      </TelegramShareButton>
+
+                      <div className="social-wrapper__share-count">&nbsp;</div>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <WhatsappShareButton
+                        url={shareUrl}
+                        title={title}
+                        separator=":: "
+                        className="social-wrapper__share-button"
+                      >
+                        <WhatsappIcon size={32} round />
+                      </WhatsappShareButton>
+
+                      <div className="social-wrapper__share-count">&nbsp;</div>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <LinkedinShareButton url={shareUrl} className="social-wrapper__share-button">
+                        <LinkedinIcon size={32} round />
+                      </LinkedinShareButton>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <PinterestShareButton
+                        url={String(window.location)}
+                        media={`${String(window.location)}/${exampleImage}`}
+                        className="social-wrapper__share-button"
+                      >
+                        <PinterestIcon size={32} round />
+                      </PinterestShareButton>
+
+                      <div>
+                        <PinterestShareCount url={shareUrl} className="social-wrapper__share-count" />
+                      </div>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <RedditShareButton
+                        url={shareUrl}
+                        title={title}
+                        windowWidth={660}
+                        windowHeight={460}
+                        className="social-wrapper__share-button"
+                      >
+                        <RedditIcon size={32} round />
+                      </RedditShareButton>
+
+                      <div>
+                        <RedditShareCount url={shareUrl} className="social-wrapper__share-count" />
+                      </div>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <TumblrShareButton
+                        url={shareUrl}
+                        title={title}
+                        className="social-wrapper__share-button"
+                      >
+                        <TumblrIcon size={32} round />
+                      </TumblrShareButton>
+
+                      <div>
+                        <TumblrShareCount url={shareUrl} className="social-wrapper__share-count" />
+                      </div>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <LivejournalShareButton
+                        url={shareUrl}
+                        title={title}
+                        description={shareUrl}
+                        className="social-wrapper__share-button"
+                      >
+                        <LivejournalIcon size={32} round />
+                      </LivejournalShareButton>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <EmailShareButton
+                        url={shareUrl}
+                        subject={title}
+                        body="body"
+                        className="social-wrapper__share-button"
+                      >
+                        <EmailIcon size={32} round />
+                      </EmailShareButton>
+                    </div>
+
+                    <div className="social-wrapper">
+                      <WorkplaceShareButton
+                        url={shareUrl}
+                        quote={title}
+                        className="social-wrapper__share-button"
+                      >
+                        <WorkplaceIcon size={32} round />
+                      </WorkplaceShareButton>
+                    </div>
+                  </div>
                 </article>
               </div>
             </div>
